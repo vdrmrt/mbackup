443bf241e7d29a8b853b0a279456f9e335c40e9d
vdrmrt <vdrmrt@f24c2318-e9b1-472b-bf2a-36b3ec02cba3>
2013-08-21 09:59:13 +0000

    added: support for db file location
    git-svn-id: svn+ssh://lambda/var/svn-repos/mbackup/trunk@64 f24c2318-e9b1-472b-bf2a-36b3ec02cba3

--- db/__init__.py
+++ db/__init__.py
@@ -1,15 +1,21 @@
 import os
 import sqlite3
 import importlib
+import ctypes.wintypes
+
+CSIDL_PERSONAL= 5        # My Documents
+CSIDL_LOCAL_APPDATA = 28 # Applicaiton Data (non roaming)
+SHGFP_TYPE_CURRENT= 0    # Want current, not default value
 
 connection = None
-dbFilename = 'mbackup.db'
 
 dbObjs = {}
 
 def getConnection():
     global connection
     if not connection:
+        dbFilename = getDbPath()
+        print(dbFilename)
         db_is_new = not os.path.exists(dbFilename)
                 
         with sqlite3.connect(dbFilename) as connection:
@@ -28,3 +34,18 @@ def getDbObj(name):
         dbObjs[name] = getattr(module,name + 'Db')()    
     
     return dbObjs[name]
+
+def createSchema():
+    print('Creating.... need to implement.')
+    
+def getDbPath():
+    buf= ctypes.create_unicode_buffer(ctypes.wintypes.MAX_PATH)
+    ctypes.windll.shell32.SHGetFolderPathW(0,CSIDL_LOCAL_APPDATA, 0, SHGFP_TYPE_CURRENT, buf)
+    appdataDir = buf.value
+    if not os.path.isdir(appdataDir):
+        raise('App data directroy does not exist ({p})'.format(p=appdataDir))
+    
+    dbDir = appdataDir + "\mbackup\db"
+    os.makedirs(dbDir,exist_ok=True)
+
+    return dbDir + '\mbackup.db'
